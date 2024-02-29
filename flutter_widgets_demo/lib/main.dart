e86902d95c8c8e05cb00a818d477900740e0ebfe
Suvankar Das <hi.suvankar@gmail.com>
2024-02-29 22:08:14 +0530

    stateful widget

--- flutter_widgets_demo/lib/main.dart
+++ flutter_widgets_demo/lib/main.dart
@@ -1,23 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const StateFulWidgetDemo());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class StateFulWidgetDemo extends StatefulWidget {
+  const StateFulWidgetDemo({Key? key}) : super(key: key);
+
+  @override
+  State<StateFulWidgetDemo> createState() => _StateFulWidgetDemoState();
+}
+
+class _StateFulWidgetDemoState extends State<StateFulWidgetDemo> {
+  double _fontSize = 20;
+
+  void increaseSize() {
+    setState(() {
+      if (_fontSize <= 40) {
+        _fontSize += 20;
+      } else {
+        _fontSize = 20;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Container(
-        child: Text(
-          "Basic template",
-          style: TextStyle(color: Colors.white),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Stateful widget demo"),
+          //backgroundColor: Colors.cyanAccent,
+        ),
+        body: Center(
+          child: TextButton(
+            onPressed: increaseSize,
+            child: Text(
+              "Hello There",
+              style: TextStyle(fontSize: _fontSize, color: Colors.black),
+            ),
+          ),
         ),
       ),
     );
